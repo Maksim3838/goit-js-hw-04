b07e0ce75e4ac4e34ef72f29a83483f5e91b9d52
Maksim <70702008m@gmail.com>
2025-08-01 22:06:01 +0300

    text

--- js/task-1.js
+++ js/task-1.js
@@ -1,26 +1,18 @@
 function isEnoughCapacity(products, containerSize) {
-  // Отримуємо всі значення (кількість кожного товару)
   const quantities = Object.values(products);
-
-  // Обчислюємо загальну кількість товарів
   const totalQuantity = quantities.reduce((total, quantity) => total + quantity, 0);
-
-  // Порівнюємо з розміром контейнера
   return totalQuantity <= containerSize;
 }
 
 console.log(
   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
-
+); 
 console.log(
   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
-
+); 
 console.log(
   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-); // true
-
+); 
 console.log(
   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-); // false
+); 
